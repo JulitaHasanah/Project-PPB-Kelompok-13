c1eb2d3f4fb30fb88569f497f1ec692c79d79b14
Ana Maria Parasanti <112605121+anamria@users.noreply.github.com>
2023-06-28 10:17:04 +0800

    Add files via upload

---
+++ App/build/generated/source/buildConfig/debug/com/example/Berita_Indonesia/Kel13/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.Berita_Indonesia.Kel13;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.Berita_Indonesia.Kel13";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String NEWS_API_TOKEN = "872a25eec33e460495f167ae049d4f08";
+}
